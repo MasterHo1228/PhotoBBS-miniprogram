25601af716f81f17a0e6e02eddec4b066dae922c
MasterHo1228 <masterho1228@gmail.com>
2022-01-05 14:06:58 +0800

    Build user show page

--- src/api/user.js
+++ src/api/user.js
@@ -4,6 +4,10 @@ export function getCurrentUser(data) {
   return authRequest('user')
 }
 
+export function getUser(id) {
+  return request('users/'+id)
+}
+
 export function updateUser(data) {
   return authRequest('user', {
     method: 'put',
