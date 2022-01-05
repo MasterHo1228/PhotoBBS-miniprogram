895eb7bdbf7c6e033f72d753e5f6305ffd2fb27a
MasterHo1228 <masterho1228@gmail.com>
2022-01-05 20:00:05 +0800

    重构动态修改标题文字的方法

---
+++ src/mixins/link-from.js
@@ -0,0 +1,39 @@
+export default {
+  data:{
+    //记录进入页面来源
+    from: null,
+    //页面标题
+    titlePrefix: null,
+    //标题关键字
+    titleKey: null,
+    //完整标题
+    fullTitle: null
+  },
+  methods:{
+    changeTitleName(){
+      if(this.from && this.titleKey){
+        //根据获取到的打开来源定义页面标题
+        switch (this.from){
+          case 'me':
+            this.titlePrefix = '我的'
+            break
+          case 'user-show':
+            this.titlePrefix = 'Ta 发布的'
+            break
+        }
+
+        //拼接标题文字并设置
+        this.fullTitle = this.titlePrefix + this.titleKey
+        wx.setNavigationBarTitle({
+          title: this.fullTitle
+        })
+      } else {
+        wx.showToast({
+          title: '非法请求！',
+          icon: 'error',
+          duration: 2000
+        })
+      }
+    }
+  }
+}
